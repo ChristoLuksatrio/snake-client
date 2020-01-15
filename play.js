d3d69975cf78270e2411a016dc6bfe75eb8e7412
Rangga <ranggaluksatrio@gmial.com>
2020-01-15 15:28:25 -0500

    snake game update

--- play.js
+++ play.js
@@ -1,26 +1,6 @@
 const { connect } = require('./client');
+const { setupInput } = require('./input');
 console.log('Connecting ...');
-connect();
 
-/**
- * Setup User Interface 
- * Specifically, so that we can handle user input via stdin
- */
-const setupInput = function(callback) {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-  stdin.on('data', callback);
-  return stdin;
 
-}
-
-const handleUserInput = (move) => {
-  if (move === '\u0003') {
-    process.exit();
-  };
-  console.log(move);
-}
-
-setupInput(handleUserInput)
+setupInput(connect());
